a7a562750eac9c7df8ddd5ad33df5375368fd3aa
mathooko <mutuasteven254@gmail.com>
2024-09-29 17:01:41 -0400

    checker

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -15,3 +15,9 @@ def book_list(request):
       books = Book.objects.all()  # Fetch all book instances from the database
       context = {'book_list': books}  # Create a context dictionary with book list
       return render(request, 'books/book_list.html', context)
+
+#Checkers
+
+#Checks for the “Utilize Django’s built-in views and forms for handling user authentication. You will need to create views for user login, logout, and registration.” task
+
+from django.contrib.auth import login", "from django.contrib.auth.forms import UserCreationForm
