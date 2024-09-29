021d9e684e565303d48078478af65831c1d24507
mathooko <mutuasteven254@gmail.com>
2024-09-29 15:44:52 -0400

    (models.Model) In the right directory

--- django-models/relationship_app/models.py
+++ django-models/relationship_app/models.py
@@ -5,7 +5,7 @@ class Author(models.Model):
     name= models.CharField(max_length=200)
     def __str__(self):
             return self.name
-class Book:
+class Book(models.Model):
     title=models.CharField(max_length=200)
     author=models.ForeignKey(Author)
 class Library:
