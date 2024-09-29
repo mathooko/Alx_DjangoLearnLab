7f7f08e89a973db2271ac35aab23400a9e155a68
mathooko <mutuasteven254@gmail.com>
2024-09-28 21:54:33 -0400

    Moving relationship app one repo to the outside. There are currently two relationship apps.

---
+++ django-models/relationship_app/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Author:
+    name= models.CharField(max_length=200)
+class Book:
+    title=models.CharField(max_length=200)
+    author=models.ForeignKey(Author)
+class Library:
+    name=models.CharField(max_length=200)
+    books=models.ManyToManyField(Book)
+class Librarian:
+    name=models.CharField(max_length=200)
+    library=models.OneToOneField(Library)
+
+    
