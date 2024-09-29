49bb2f7c351911d6199f7c1af62074f283deda01
mathooko <mutuasteven254@gmail.com>
2024-09-28 21:35:15 -0400

    Templates added(Not sure if this is the right place to put). Views added with a bit of code

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.http import HttpResponse
+
+def hello_view(request):
+    """A basic function view returning a greeting message."""
+    return HttpResponse("Hello, World!")
+
+from django.shortcuts import render
+from .models import Book
+
+def book_list(request):
+      """Retrieves all books and renders a template displaying the list."""
+      books = Book.objects.all()  # Fetch all book instances from the database
+      context = {'book_list': books}  # Create a context dictionary with book list
+      return render(request, 'books/book_list.html', context)
