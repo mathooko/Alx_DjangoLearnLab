7e847af92be8f04146d2ed9b257e7bce52cfea42
mathooko <mutuasteven254@gmail.com>
2024-10-14 12:06:46 -0400

    Check for settings.py Configuration:

--- advanced_features_and_security/LibraryProject/LibraryProject/settings.py
+++ advanced_features_and_security/LibraryProject/LibraryProject/settings.py
@@ -123,3 +123,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# Check for settings.py Configuration:
+
+"bookshelf.CustomUser"
